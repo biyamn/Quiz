13606440ba20b00e5f5567645fb80f2022c7835c
HamZik <b1y9mn@gmail.com>
2023-07-25 21:53:49 +0900

    chore: 확장자 변경

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+// 일단 '!'로 강제로 넣어줌
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+)
