be506fd609d74c9824c91f6ad48808ad6485863b
HamZik <b1y9mn@gmail.com>
2023-07-26 13:36:26 +0900

    chore: import시 tsx 확장자 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 
 // 일단 '!'로 강제로 넣어줌
 ReactDOM.createRoot(document.getElementById('root')!).render(
