ff2852eb02fa91d4e720626e431fa231e0d960ff
HamZik <b1y9mn@gmail.com>
2023-07-28 16:43:39 +0900

    chore: lint 관련 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
-    "eslint:recommended",
+    "airbnb",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react", "react-hooks", "jsx-a11y", "import", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react-refresh/only-export-components": "warn",
